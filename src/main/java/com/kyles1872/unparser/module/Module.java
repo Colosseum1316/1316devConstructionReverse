6d970c09ca64bedfee186956850ab9fa456c7887
Kyle S <34375742+KyleS1872@users.noreply.github.com>
2024-01-22 19:50:44 +0000

    Initial commit

---
+++ src/main/java/com/kyles1872/unparser/module/Module.java
@@ -0,0 +1,15 @@
+package com.kyles1872.unparser.module;
+
+import org.bukkit.plugin.Plugin;
+
+/**
+ * @author Kyle
+ */
+public class Module {
+
+  protected final Plugin plugin;
+
+  public Module(Plugin plugin) {
+    this.plugin = plugin;
+  }
+}
