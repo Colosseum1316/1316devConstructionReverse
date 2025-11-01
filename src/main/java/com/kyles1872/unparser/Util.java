76f7d6d0b366bd2409e6872658b632e84146c714
NorthRealm <155140859+NorthRealm@users.noreply.github.com>
2025-11-01 16:51:44 +0800

    FORK

---
+++ src/main/java/com/kyles1872/unparser/Util.java
@@ -0,0 +1,16 @@
+package com.kyles1872.unparser;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Colosseum1316
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+final class Util {
+  static final short MATERIAL_WOOL = (short) 35;
+  static final short MATERIAL_SPONGE = (short) 19;
+  static final short MATERIAL_GOLD_PLATE = (short) 147;
+  static final short MATERIAL_IRON_PLATE = (short) 148;
+  static final short MATERIAL_SIGN_POST = (short) 63;
+}
