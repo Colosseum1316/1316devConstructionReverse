a7850f16571b4729676fa6037867a6286d0ee12f
NorthRealm <155140859+NorthRealm@users.noreply.github.com>
2025-11-03 23:46:54 +0800

    FORK

---
+++ src/main/java/colosseum/unparser/Block.java
@@ -0,0 +1,36 @@
+package colosseum.unparser;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Colosseum1316
+ */
+@SuppressWarnings("FieldMayBeFinal")
+public final class Block {
+  @Getter
+  @Setter
+  private short id;
+  @Getter
+  @Setter
+  private byte data;
+  private Location location;
+
+  public Block(short id, byte data, int x, int y, int z) {
+    this.id = id;
+    this.data = data;
+    this.location = new Location(x, y, z);
+  }
+
+  public int getX() {
+    return location.getBlockX();
+  }
+
+  public int getY() {
+    return location.getBlockY();
+  }
+
+  public int getZ() {
+    return location.getBlockZ();
+  }
+}
---
+++ src/main/java/com/kyles1872/unparser/MapData.java
@@ -0,0 +1,40 @@
+package com.kyles1872.unparser;
+
+import colosseum.unparser.Location;
+import colosseum.utility.arcade.GameType;
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Modified by Colosseum1316
+ *
+ * @author Kyle
+ */
+@AllArgsConstructor
+final class MapData {
+
+  public final String name;
+  public final String author;
+  public final GameType gameType;
+
+  public final Location[] corners;
+
+  public final HashMap<String, ArrayList<Location>> dataLocations;
+  public final HashMap<String, ArrayList<Location>> teamLocsLocations;
+  public final HashMap<String, ArrayList<Location>> customLocsLocations;
+
+  public List<Location> getIronLocations(String key) {
+    return dataLocations.computeIfAbsent(key, k -> new ArrayList<>());
+  }
+
+  public List<Location> getGoldLocations(String key) {
+    return teamLocsLocations.computeIfAbsent(key, k -> new ArrayList<>());
+  }
+
+  public List<Location> getSpongeLocations(String key) {
+    return customLocsLocations.computeIfAbsent(key, k -> new ArrayList<>());
+  }
+}
